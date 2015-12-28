0ecfeb7e17657a757d4a5342d7d848b4251fa030
Max Malm <benjick@dumfan.net>
2015-12-28 13:11:47 +0100

    Better performance

--- grav/docker-entrypoint.sh
+++ grav/docker-entrypoint.sh
@@ -2,21 +2,11 @@
 set -e
 shopt -s extglob
 
-if ! [ -e index.php -a -e bin/gpm ]; then
-	echo >&2 "Grav not found in $(pwd) - copying now..."
-	if [ "$(ls -A)" ]; then
-		echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
-		( set -x; ls -A; sleep 10 )
-	fi
-  rsync -a /tmp/grav/ /var/www/html --exclude user
-  if ! [ -e user/config/system.yaml ]; then
-    echo >&2 "No userfiles found - copying now..."
-    rsync -a /tmp/grav/user/ /var/www/html/user
-  fi
-  /var/www/html/bin/grav install
-  chown -R www-data:www-data !(user)
-  chown -R www-data:www-data user || true
-	echo >&2 "Complete! Grav has been successfully installed"
+if ! [ -e user/config/system.yaml ]; then
+  echo >&2 "No userfiles found - copying now..."
+  rsync -a /tmp/grav-$GRAV_VERSION/user/ /var/www/html/user
 fi
+chown -Rf www-data:www-data user || true
+echo >&2 "Complete! Grav has been successfully installed"
 
 exec "$@"
